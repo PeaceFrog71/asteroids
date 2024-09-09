565b93414673158d722f29c9415fe1c0a3fcb97c
PeaceFrog71 <dafrog71@yahoo.com>
2024-09-09 04:56:01 -0600

    Refactor constants.py to add PLAYER_RADIUS and WHITE color constant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -12,13 +13,21 @@ def main():
 	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	running = True
+
+	player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(BLACK)
+		player.draw(screen)
 		pygame.display.flip()
 		dt = game_clock.tick(60)/1000
+
+		
+
+		
 
 	pygame.quit()
 	 
